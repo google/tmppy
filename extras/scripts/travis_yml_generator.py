74248bc21658d20ff80ce116b2371423787b8b00
Marco Poletti <poletti.marco@gmail.com>
2017-11-26 14:52:23 +0000

    Switch back to Clang 4.0 (instead of 5.0) when testing on Ubuntu 17.04.

--- extras/scripts/travis_yml_generator.py
+++ extras/scripts/travis_yml_generator.py
@@ -106,7 +106,7 @@ add_ubuntu_tests(ubuntu_version='17.10', compiler='gcc-7', smoke_tests=['DebugPl
 add_ubuntu_tests(ubuntu_version='17.10', compiler='clang-5.0', stl='libstdc++', smoke_tests=['DebugPlain', 'ReleasePlain'])
 
 add_ubuntu_tests(ubuntu_version='17.04', compiler='gcc-6', smoke_tests=['DebugPlain', 'ReleasePlain'])
-add_ubuntu_tests(ubuntu_version='17.04', compiler='clang-5.0', stl='libstdc++', smoke_tests=['DebugPlain', 'ReleasePlain'])
+add_ubuntu_tests(ubuntu_version='17.04', compiler='clang-4.0', stl='libstdc++', smoke_tests=['DebugPlain', 'ReleasePlain'])
 
 add_ubuntu_tests(ubuntu_version='16.04', compiler='gcc-5')
 add_ubuntu_tests(ubuntu_version='16.04', compiler='clang-3.5', stl='libstdc++')
