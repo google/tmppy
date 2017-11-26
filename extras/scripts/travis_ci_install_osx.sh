e69e0cbbb93399255adb0317d3fb15ba0d26d476
Marco Poletti <poletti.marco@gmail.com>
2017-11-26 14:04:57 +0000

    Install typed_ast when testing TMPPy in Travis CI on OS X.

--- extras/scripts/travis_ci_install_osx.sh
+++ extras/scripts/travis_ci_install_osx.sh
@@ -37,6 +37,7 @@ esac
 time brew install python3
 time pip3 install pytest
 time pip3 install pytest-xdist
+time pip3 install typed_ast
 
 # This adds python-installed executables to PATH (notably py.test).
 export PATH="$(brew --prefix)/bin:$PATH"
